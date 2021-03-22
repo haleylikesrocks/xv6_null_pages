d10dc1f42b90e9c3ba613f9bf764815272e91a41
Haley <ramirezhaley@gmail.com>
2021-03-22 11:46:20 -0500

    starting project

---
+++ testnull.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#define NULL ((int *)0)
+
+int main(int argc, char *argv[])
+{
+
+
+    printf(1, "Null pointer value :%d \n", *NULL);
+    exit(); 
+}
